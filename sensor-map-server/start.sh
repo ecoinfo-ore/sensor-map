2c477b3a823a88e6b5709740aaa6767dede26cab
ecoinfo-ore <37690220+ecoinfo-ore@users.noreply.github.com>
2020-04-03 18:08:39 +0200

    Update start.sh

--- sensor-map-server/start.sh
+++ sensor-map-server/start.sh
@@ -4,14 +4,14 @@
  
  cd $CURRENT_PATH
 
- java -jar sensor-map/sensor-map-0.9.jar &
+ # java -jar sensor-map/sensor-map-0.9.jar &
  
- # java -Dquarkus.datasource.driver=org.postgresql.Driver              \
- #      -Dquarkus.datasource.url=jdbc:postgresql://localhost:5432/coby \
- #      -Dquarkus.hibernate-orm.database.generation=none               \
- #      -Dquarkus.datasource.username=ryahiaoui                        \
- #      -Dquarkus.datasource.password=yahiaoui                         \
- #      -Dquarkus.datasource.jdbc.min-size=4                           \
- #      -Dquarkus.datasource.jdbc.max-size=16                          \
- #      -jar sensor-map/sensor-map-0.9.jar &
+  java -Dquarkus.datasource.driver=org.postgresql.Driver                     \
+       -Dquarkus.datasource.url=jdbc:postgresql://localhost:2345/sensorusers \
+       -Dquarkus.hibernate-orm.database.generation=none                      \
+       -Dquarkus.datasource.username=postgres                                \
+       -Dquarkus.datasource.password=postgres                                \
+       -Dquarkus.datasource.jdbc.min-size=4                                  \
+       -Dquarkus.datasource.jdbc.max-size=16                                 \
+       -jar sensor-map/sensor-map-0.9.jar  &
 
